528e60ee4b2a55838bcfe86be6b6e0f219f6f5ea
imprint <asiwebrightemeka@gmail.com>
2022-08-26 12:05:16 -0700

    fixed database

--- Backend/Config/DbConnect.js
+++ Backend/Config/DbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
    try {
-     const conn = await  mongoose.connect(process.env.URI,{
+     const conn = await  mongoose.connect(process.env.DATABASE_URL,{
                 useUnifiedTopology: true,
                 useNewUrlParser: true,
                 // useCreateIndex: true
